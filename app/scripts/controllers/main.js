221ee80dd0a84d62212d5b816d2b3218cbc97e1d
juliausti <julian.ustiyanovych@gmail.com>
2014-10-14 17:34:10 +0300

    add fix when client bidding - do not add bid manualy, instead whait until last:bid event will be raised.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,9 @@
 
 angular.module('clientbiddingApp')
   .controller('MainCtrl', function ($scope, socketService) {
-  		$scope.userName = 'User_' + Math.floor((Math.random() * 10000) + 1)
-  		$scope.allBids = [];
-  		$scope.users = [];
+		$scope.userName = 'User_' + Math.floor((Math.random() * 10000) + 1)
+		$scope.allBids = [];
+		$scope.users = [];
 		$scope.bid = 0;
 		$scope.highestBid = 0;
 		$scope.name = '';
@@ -12,32 +12,9 @@ angular.module('clientbiddingApp')
 		$scope.description = '';
 		$scope.placeBid = placeBid;
 
-		// place my bid on the item
 		function placeBid() {
-			$scope.allBids.splice(0, 0, {name: $scope.userName , bid: $scope.bid});
 			socketService.emit('place:bid', $scope.bid);
-			// if($scope.allBids.length > 0) {
-			// 	if($scope.bid > $scope.highestBid) {
-			// 		$scope.allBids.splice(0, 0, {name: $scope.userName , bid: $scope.bid});
-			// 		socketService.emit('place:bid', $scope.bid);
-			// 	} else {
-			// 		alert('You cannot place bid lower than highest bid');
-			// 	}
-			// } else {
-			// 	$scope.allBids.splice(0, 0, {name: $scope.userName , bid: $scope.bid});
-			// 	socketService.emit('place:bid', $scope.bid);
-			// }
 		}
-
-		function _inArray(list, value) {
-			for (var i = 0; i < list.length; i++) {
-				if (list[i].bid === value) {
-					return true;
-				}
-			}
-			return false;
-		}
-
 		socketService.on('connect', function () {
 
 			socketService.emit('join', $scope.userName);
@@ -54,4 +31,4 @@ angular.module('clientbiddingApp')
 				$scope.users.push(user);
 			});
 		});
-  });
+	});
